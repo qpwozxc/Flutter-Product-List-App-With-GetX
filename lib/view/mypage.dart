d9d1c8c509c3bd7dc875b93019aec2359733b992
김성중 <ksj@MacBookAir.local>
2024-01-30 14:38:59 +0900

    :white_check_mark: Update view

--- lib/view/mypage.dart
+++ lib/view/mypage.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_practice/controller/controller.dart';
+import 'package:getx_practice/view/product_tile.dart';
 
 class MyPage extends StatelessWidget {
-  const MyPage({super.key});
+  MyPage({super.key});
+
+  final controller = Get.put(Controller());
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +26,27 @@ class MyPage extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
           ),
         ],
+      ),
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+          child: Obx(
+            () => GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+              ), // Gridview의 모양을 결정
+
+              itemBuilder: (context, index) {
+                return ProductTile(
+                  controller.productList[index],
+                );
+              },
+              itemCount: controller.productList.length,
+            ),
+          ),
+        ),
       ),
     );
   }
